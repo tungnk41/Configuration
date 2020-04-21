cf8135a818e6ec37d37d941a42921e3bf0ffe819
Tung Nguyen <tung.nk41@gmail.com>
2020-04-21 22:43:27 +0700

    add qt config

--- git.sh
+++ git.sh
@@ -2,4 +2,12 @@
 git config --global user.name "Tung Nguyen"
 git config --global user.email tung.nk41@gmail.com
 git config --global core.editor "vim"
+
+#Test ssh
 #ssh -T git@github.com
+
+#Create Repo
+#git init
+#git remote add $RepoName $LinkGitRepoServer
+#git add $file
+#git push $RepoName $Branch
