ce4c1ad772a3625008c17e8e0636e68abd236c87
Tung Nguyen <tung.nk41@gmail.com>
2020-09-10 22:39:53 +0700

    add device adb

--- adb.sh
+++ adb.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 PlatformTools="/home/tuhn/Android/Sdk/platform-tools"
-IP="192.168.123.123"
+IP1="192.168.123.123"
+IP2="192.168.123.124"
 PORT="5555"
 
 export PATH=$PlatformTools:$PATH
 adb tcpip $PORT
-adb connect $IP:$PORT
+adb connect $IP1:$PORT
+adb connect $IP2:$PORT
 clear
 adb devices
 exit
