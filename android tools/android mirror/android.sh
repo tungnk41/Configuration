0ddb0ea06f6a14e56e6bd83d6ec7000c4c90c43e
Tung Nguyen <tung.nk41@gmail.com>
2021-06-13 10:21:15 +0700

    add android mirror tool

---
+++ android tools/android mirror/android.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+xdotool windowminimize $(xdotool getactivewindow)
+scrcpy -m 960 -b 20M --max-fps 60
---
+++ android tools/android mirror/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+chmod +x android.sh android.desktop
+yes | cp -rf android.desktop ~/.local/share/applications
+yes | cp -rf Android-Mirror.png ~/Pictures
