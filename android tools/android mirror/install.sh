1dac27985bc468ebd3c827bc4452dddf5e916bdd
Tung Nguyen <tung.nk41@gmail.com>
2021-06-27 17:49:21 +0700

    update file

--- android tools/android mirror/install.sh
+++ android tools/android mirror/install.sh
@@ -2,10 +2,10 @@
 
 #require install vlc,xdotool
 
-chmod +x MirrorScreen MirrorScreen.desktop fwdAudio
+chmod +x MirrorScreen MirrorScreen.desktop audioAndroidFWD
 yes | cp -rf MirrorScreen.desktop ~/.local/share/applications
 yes | cp -rf Android-Mirror.png ~/Pictures
-yes | cp -rf fwdAudio ~/.bin
+yes | cp -rf audioAndroidFWD ~/.bin
 
 # ~.profile => 
 #export ADB=$HOME/Android/Sdk/platform-tools/adb scrcpy
